d517948b44f082e7bdcb6130dc960326de19f26a
Rachel Hertel <rghertel29@gmail.com>
2021-02-20 11:25:46 -0600

    final code

--- jobs_project.sql
+++ jobs_project.sql
@@ -24,8 +24,8 @@ WHERE location = 'TN' OR location = 'KY';
 --4.
 SELECT COUNT(*)
 FROM data_analyst_jobs
-WHERE location = 'TN' AND star_rating >= 4;
---4
+WHERE location = 'TN' AND star_rating > 4;
+--3
 
 --5.
 SELECT COUNT(*)
@@ -36,6 +36,8 @@ WHERE review_count BETWEEN 500 AND 1000;
 --6.
 SELECT location AS state, ROUND(AVG(star_rating),2) AS avg_rating
 FROM data_analyst_jobs
+WHERE location IS NOT NULL
+AND star_rating IS NOT NULL
 GROUP BY state
 ORDER BY avg_rating DESC;
 --Nebraska
@@ -62,10 +64,10 @@ WHERE review_count > 5000
 GROUP BY company;
 
 --9b.
-SELECT COUNT(company)
+SELECT COUNT(DISTINCT(company))
 FROM data_analyst_jobs
-WHERE review_count > 5000
---184
+WHERE review_count > 5000;
+--40
 
 --10.
 SELECT company, ROUND(AVG(star_rating),1) AS avg_rating
@@ -79,22 +81,33 @@ ORDER BY avg_rating DESC;
 --11a.
 SELECT title
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%';
+WHERE title ILIKE '%Analyst%';
 
 --11b.
 SELECT COUNT(DISTINCT(title))
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%';
---754
+WHERE title ILIKE '%Analyst%';
+--774
 
---12a.
+--12.
 SELECT COUNT(DISTINCT(title))
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%' OR title NOT LIKE '%Analytics%';
---802
+WHERE title NOT ILIKE '%Analyst%' AND title NOT ILIKE '%Analytics%'
+--4
 
 --12b.
 SELECT DISTINCT(title)
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst' AND title NOT LIKE '%Analytics';
+WHERE title NOT ILIKE '%Analyst%' AND title NOT ILIKE '%Analytics%'
+--They all involve Tableau
 
+--BONUS.
+SELECT COUNT(title) AS open_jobs_count, domain AS industry
+FROM data_analyst_jobs
+WHERE skill ILIKE '%SQL%'
+AND days_since_posting > 21
+AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY open_jobs_count DESC;
+--Internet and Software: 62, Banks and Financial Services: 61, 
+--Consulting and Business Services: 57, Healthcare: 52
